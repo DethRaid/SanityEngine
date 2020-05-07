c76d5ac7cbfb6a246d2c5d0ab054df9ef7bd4289
David Dubois <dexcelstraun7@gmail.com>
2020-05-07 14:41:59 -0700

    Don't need to explicitly delete the user error data

--- SanityEngine/src/bve/bve_wrapper.cpp
+++ SanityEngine/src/bve/bve_wrapper.cpp
@@ -8,14 +8,6 @@ BveMeshHandle BveWrapper::load_mesh_from_file(const std::string& filename) {
     return BveMeshHandle{mesh, bve::bve_delete_loaded_static_mesh};
 }
 
-BveErrorDataHandle BveWrapper::get_printable_error(const bve::BVE_Mesh_Error& error) {
-    auto* data = new bve::BVE_User_Error_Data;
-    *data = bve::BVE_Mesh_Error_to_data(&error);
-
-    return BveErrorDataHandle{data, [](bve::BVE_User_Error_Data* error_data) {
-                                  bve::bve_delete_string(error_data->description);
-                                  bve::bve_delete_string(error_data->description_english);
-
-                                  delete error_data;
-                              }};
+BVE_User_Error_Data BveWrapper::get_printable_error(const bve::BVE_Mesh_Error& error) {
+    return bve::BVE_Mesh_Error_to_data(&error);
 }
