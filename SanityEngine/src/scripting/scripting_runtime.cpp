fb6b38aa26a3df35880bf012cd2ecd9709002c75
David Dubois <dexcelstraun7@gmail.com>
2020-06-05 22:25:01 -0700

    Add the start of somehting beautiful

--- SanityEngine/src/scripting/scripting_runtime.cpp
+++ SanityEngine/src/scripting/scripting_runtime.cpp
@@ -1,3 +1,78 @@
 #include "scripting_runtime.hpp"
 
-#include "hostfxr.h"
+#include <filesystem>
+
+#include <Windows.h>
+#include <hostfxr.h>
+#include <nethost.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+
+#include "../core/ensure.hpp"
+
+std::shared_ptr<spdlog::logger> ScriptingRuntime::logger{spdlog::stdout_color_st("ScriptingRuntime")};
+
+hostfxr_initialize_for_runtime_config_fn init_fptr;
+hostfxr_get_runtime_delegate_fn get_delegate_fptr;
+hostfxr_close_fn close_fptr;
+
+load_assembly_and_get_function_pointer_fn get_dotnet_assembly_fptr;
+
+[[nodiuscard]] bool load_hostfxr() {
+    char_t buffer[MAX_PATH];
+    auto buffer_size = sizeof(buffer) / sizeof(char_t);
+    const auto rc = get_hostfxr_path(buffer, &buffer_size, nullptr);
+    if(rc != 0) {
+        return false;
+    }
+
+    // Load hostfxr and get desired exports
+
+    auto* lib = LoadLibraryW(buffer);
+    init_fptr = reinterpret_cast<hostfxr_initialize_for_runtime_config_fn>(GetProcAddress(lib, "hostfxr_initialize_for_runtime_config"));
+    get_delegate_fptr = reinterpret_cast<hostfxr_get_runtime_delegate_fn>(GetProcAddress(lib, "hostfxr_get_runtime_delegate"));
+    close_fptr = reinterpret_cast<hostfxr_close_fn>(GetProcAddress(lib, "hostfxr_close"));
+
+    return (init_fptr && get_delegate_fptr && close_fptr);
+}
+
+// Most of the code here comes from Microsoft's custom app host samples
+
+std::optional<ScriptingRuntime> ScriptingRuntime::create(const std::string& assembly_path, const std::string& assembly_name) {
+    if(!load_hostfxr()) {
+        logger->error("Could not load HostFXR");
+    }
+
+    const auto config_path = std::filesystem::path{assembly_path} / assembly_path / ".json";
+    if(exists(config_path)) {
+        const load_assembly_and_get_function_pointer_fn load_assembly_and_get_function_pointer_local = get_dotnet_load_assembly(config_path);
+        ENSURE(load_assembly_and_get_function_pointer_local != nullptr, "Failure: get_dotnet_load_assembly()");
+
+        return std::make_optional<ScriptingRuntime>(load_assembly_and_get_function_pointer_local);
+    }
+
+    return std::nullopt;
+}
+
+load_assembly_and_get_function_pointer_fn ScriptingRuntime::get_dotnet_load_assembly(const std::filesystem::path& config_path) {
+    // Load .NET Core
+    void* load_assembly_and_get_function_pointer = nullptr;
+    hostfxr_handle cxt = nullptr;
+    int rc = init_fptr(config_path.c_str(), nullptr, &cxt);
+    if(rc != 0 || cxt == nullptr) {
+        logger->error("Init failed: {#x}", rc);
+        close_fptr(cxt);
+        return nullptr;
+    }
+
+    // Get the load assembly function pointer
+    rc = get_delegate_fptr(cxt, hdt_load_assembly_and_get_function_pointer, &load_assembly_and_get_function_pointer);
+    if(rc != 0 || load_assembly_and_get_function_pointer == nullptr) {
+        logger->error("Get delegate failed: {#x}", rc);
+    }
+
+    close_fptr(cxt);
+    return static_cast<load_assembly_and_get_function_pointer_fn>(load_assembly_and_get_function_pointer);
+}
+
+ScriptingRuntime::ScriptingRuntime(const load_assembly_and_get_function_pointer_fn load_assembly_and_get_function_pointer_in)
+    : load_assembly_and_get_function_pointer{load_assembly_and_get_function_pointer_in} {}
--- SanityEngine/src/scripting/scripting_runtime.hpp
+++ SanityEngine/src/scripting/scripting_runtime.hpp
@@ -1,10 +1,38 @@
 #pragma once
 
+#include <fstream>
+#include <optional>
+#include <string>
+
+#include <coreclr_delegates.h>
+#include <spdlog/logger.h>
+
+class ScriptClass {};
+
 class ScriptingRuntime {
 public:
-    ScriptingRuntime();
+    /*!
+     * \brief Creates a new ScriptingRuntime instance
+     *
+     * \param assembly_path The path to the scripting assembly which contains all the script code you want to use
+     * \param assembly_name The name of the assembly which contains all your scripting code
+     */
+    static std::optional<ScriptingRuntime> create(const std::string& assembly_path, const std::string& assembly_name);
 
+    ScriptingRuntime(const ScriptingRuntime& other) = default;
+    ScriptingRuntime& operator=(const ScriptingRuntime& other) = default;
 
+    ScriptingRuntime(ScriptingRuntime&& old) noexcept = default;
+    ScriptingRuntime& operator=(ScriptingRuntime&& old) noexcept = default;
 
-    ~ScriptingRuntime();
+    [[nodiscard]] ScriptClass load_scripting_class(const std::string& class_name);
+
+private:
+    static std::shared_ptr<spdlog::logger> logger;
+
+    static load_assembly_and_get_function_pointer_fn get_dotnet_load_assembly(const std::filesystem::path& config_path);
+
+    load_assembly_and_get_function_pointer_fn load_assembly_and_get_function_pointer{};
+
+    explicit ScriptingRuntime(load_assembly_and_get_function_pointer_fn load_assembly_and_get_function_pointer_in);
 };
