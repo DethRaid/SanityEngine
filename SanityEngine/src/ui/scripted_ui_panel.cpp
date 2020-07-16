8937aedd1e2475ca65afd63681f1141122b08760
David Dubois <dexcelstraun7@gmail.com>
2020-07-15 23:55:08 -0700

    Delete Wren

--- SanityEngine/src/ui/scripted_ui_panel.cpp
+++ SanityEngine/src/ui/scripted_ui_panel.cpp
@@ -1,21 +1,19 @@
 #include "scripted_ui_panel.hpp"
 
 #include "rx/core/log.h"
-#include "scripting/scripting_runtime.hpp"
-#include "wren.hpp"
 
 namespace ui {
-    RX_LOG("ScriptedUiPanel", logger);
-
-    ScriptedUiPanel::ScriptedUiPanel(WrenHandle* wren_handle_in, script::ScriptingRuntime& runtime_in)
-        : runtime{&runtime_in}, wren_handle{wren_handle_in}, script_draw_method{wrenMakeCallHandle(runtime->get_vm(), "draw()")} {}
-
-    void ScriptedUiPanel::draw() {
-        auto* vm = runtime->get_vm();
-
-        wrenEnsureSlots(vm, 1);
-        wrenSetSlotHandle(vm, 0, wren_handle);
-
-        wrenCall(vm, script_draw_method);
-    }
+    // RX_LOG("ScriptedUiPanel", logger);
+    // 
+    // ScriptedUiPanel::ScriptedUiPanel(WrenHandle* wren_handle_in, script::ScriptingRuntime& runtime_in)
+    //     : runtime{&runtime_in}, wren_handle{wren_handle_in}, script_draw_method{wrenMakeCallHandle(runtime->get_vm(), "draw()")} {}
+    // 
+    // void ScriptedUiPanel::draw() {
+    //     auto* vm = runtime->get_vm();
+    // 
+    //     wrenEnsureSlots(vm, 1);
+    //     wrenSetSlotHandle(vm, 0, wren_handle);
+    // 
+    //     wrenCall(vm, script_draw_method);
+    // }
 } // namespace ui
