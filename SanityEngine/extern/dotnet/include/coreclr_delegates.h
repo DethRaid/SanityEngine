bb2371deef42a5b7a199e6fea94cd3918848bd0e
David Dubois <dexcelstraun7@gmail.com>
2020-07-19 12:34:57 -0700

    Use dotnet files in the extern folder

--- SanityEngine/extern/dotnet/include/coreclr_delegates.h
+++ SanityEngine/extern/dotnet/include/coreclr_delegates.h
@@ -20,15 +20,15 @@ typedef char char_t;
 #endif
 
 // Signature of delegate returned by coreclr_delegate_type::load_assembly_and_get_function_pointer
-typedef int (CORECLR_DELEGATE_CALLTYPE *load_assembly_and_get_function_pointer_fn)(
-    const char_t *assembly_path      /* Fully qualified path to assembly */,
-    const char_t *type_name          /* Assembly qualified type name */,
-    const char_t *method_name        /* Public static method name compatible with delegateType */,
-    const char_t *delegate_type_name /* Assembly qualified delegate type name or null */,
-    void         *reserved           /* Extensibility parameter (currently unused and must be 0) */,
-    /*out*/ void **delegate          /* Pointer where to store the function pointer result */);
+typedef int(CORECLR_DELEGATE_CALLTYPE* load_assembly_and_get_function_pointer_fn)(
+    const char_t* assembly_path /* Fully qualified path to assembly */,
+    const char_t* type_name /* Assembly qualified type name */,
+    const char_t* method_name /* Public static method name compatible with delegateType */,
+    const char_t* delegate_type_name /* Assembly qualified delegate type name or null */,
+    void* reserved /* Extensibility parameter (currently unused and must be 0) */,
+    /*out*/ void** delegate /* Pointer where to store the function pointer result */);
 
 // Signature of delegate returned by load_assembly_and_get_function_pointer_fn when delegate_type_name == null (default)
-typedef int (CORECLR_DELEGATE_CALLTYPE *component_entry_point_fn)(void *arg, int32_t arg_size_in_bytes);
+typedef int(CORECLR_DELEGATE_CALLTYPE* component_entry_point_fn)(void* arg, int32_t arg_size_in_bytes);
 
 #endif // __CORECLR_DELEGATES_H__
--- SanityEngine/extern/dotnet/include/hostfxr.h
+++ SanityEngine/extern/dotnet/include/hostfxr.h
@@ -9,19 +9,18 @@
 #include <stdint.h>
 
 #if defined(_WIN32)
-    #define HOSTFXR_CALLTYPE __cdecl
-    #ifdef _WCHAR_T_DEFINED
-        typedef wchar_t char_t;
-    #else
-        typedef unsigned short char_t;
-    #endif
+#define HOSTFXR_CALLTYPE __cdecl
+#ifdef _WCHAR_T_DEFINED
+typedef wchar_t char_t;
 #else
-    #define HOSTFXR_CALLTYPE
-    typedef char char_t;
+typedef unsigned short char_t;
+#endif
+#else
+#define HOSTFXR_CALLTYPE
+typedef char char_t;
 #endif
 
-enum hostfxr_delegate_type
-{
+enum hostfxr_delegate_type {
     hdt_com_activation,
     hdt_load_in_memory_assembly,
     hdt_winrt_activation,
@@ -30,55 +29,44 @@ enum hostfxr_delegate_type
     hdt_load_assembly_and_get_function_pointer
 };
 
-typedef int32_t(HOSTFXR_CALLTYPE *hostfxr_main_fn)(const int argc, const char_t **argv);
-typedef int32_t(HOSTFXR_CALLTYPE *hostfxr_main_startupinfo_fn)(
-    const int argc,
-    const char_t **argv,
-    const char_t *host_path,
-    const char_t *dotnet_root,
-    const char_t *app_path);
+typedef int32_t(HOSTFXR_CALLTYPE* hostfxr_main_fn)(const int argc, const char_t** argv);
+typedef int32_t(HOSTFXR_CALLTYPE* hostfxr_main_startupinfo_fn)(
+    const int argc, const char_t** argv, const char_t* host_path, const char_t* dotnet_root, const char_t* app_path);
 
-typedef void(HOSTFXR_CALLTYPE *hostfxr_error_writer_fn)(const char_t *message);
-typedef hostfxr_error_writer_fn(HOSTFXR_CALLTYPE *hostfxr_set_error_writer_fn)(hostfxr_error_writer_fn error_writer);
+typedef void(HOSTFXR_CALLTYPE* hostfxr_error_writer_fn)(const char_t* message);
+typedef hostfxr_error_writer_fn(HOSTFXR_CALLTYPE* hostfxr_set_error_writer_fn)(hostfxr_error_writer_fn error_writer);
 
 typedef void* hostfxr_handle;
-typedef struct hostfxr_initialize_parameters
-{
+struct hostfxr_initialize_parameters {
     size_t size;
-    const char_t *host_path;
-    const char_t *dotnet_root;
-} hostfxr_initialize_parameters;
+    const char_t* host_path;
+    const char_t* dotnet_root;
+};
 
-typedef int32_t(HOSTFXR_CALLTYPE *hostfxr_initialize_for_dotnet_command_line_fn)(
-    int argc,
-    const char_t **argv,
-    const hostfxr_initialize_parameters *parameters,
-    /*out*/ hostfxr_handle *host_context_handle);
-typedef int32_t(HOSTFXR_CALLTYPE *hostfxr_initialize_for_runtime_config_fn)(
-    const char_t *runtime_config_path,
-    const hostfxr_initialize_parameters*parameters,
-    /*out*/ hostfxr_handle *host_context_handle);
+typedef int32_t(HOSTFXR_CALLTYPE* hostfxr_initialize_for_dotnet_command_line_fn)(int argc,
+                                                                                 const char_t** argv,
+                                                                                 const struct hostfxr_initialize_parameters* parameters,
+                                                                                 /*out*/ hostfxr_handle* host_context_handle);
+typedef int32_t(HOSTFXR_CALLTYPE* hostfxr_initialize_for_runtime_config_fn)(const char_t* runtime_config_path,
+                                                                            const struct hostfxr_initialize_parameters* parameters,
+                                                                            /*out*/ hostfxr_handle* host_context_handle);
 
-typedef int32_t(HOSTFXR_CALLTYPE *hostfxr_get_runtime_property_value_fn)(
-    const hostfxr_handle host_context_handle,
-    const char_t *name,
-    /*out*/ const char_t **value);
-typedef int32_t(HOSTFXR_CALLTYPE *hostfxr_set_runtime_property_value_fn)(
-    const hostfxr_handle host_context_handle,
-    const char_t *name,
-    const char_t *value);
-typedef int32_t(HOSTFXR_CALLTYPE *hostfxr_get_runtime_properties_fn)(
-    const hostfxr_handle host_context_handle,
-    /*inout*/ size_t * count,
-    /*out*/ const char_t **keys,
-    /*out*/ const char_t **values);
+typedef int32_t(HOSTFXR_CALLTYPE* hostfxr_get_runtime_property_value_fn)(const hostfxr_handle host_context_handle,
+                                                                         const char_t* name,
+                                                                         /*out*/ const char_t** value);
+typedef int32_t(HOSTFXR_CALLTYPE* hostfxr_set_runtime_property_value_fn)(const hostfxr_handle host_context_handle,
+                                                                         const char_t* name,
+                                                                         const char_t* value);
+typedef int32_t(HOSTFXR_CALLTYPE* hostfxr_get_runtime_properties_fn)(const hostfxr_handle host_context_handle,
+                                                                     /*inout*/ size_t* count,
+                                                                     /*out*/ const char_t** keys,
+                                                                     /*out*/ const char_t** values);
 
-typedef int32_t(HOSTFXR_CALLTYPE *hostfxr_run_app_fn)(const hostfxr_handle host_context_handle);
-typedef int32_t(HOSTFXR_CALLTYPE *hostfxr_get_runtime_delegate_fn)(
-    const hostfxr_handle host_context_handle,
-    enum hostfxr_delegate_type type,
-    /*out*/ void **delegate);
+typedef int32_t(HOSTFXR_CALLTYPE* hostfxr_run_app_fn)(const hostfxr_handle host_context_handle);
+typedef int32_t(HOSTFXR_CALLTYPE* hostfxr_get_runtime_delegate_fn)(const hostfxr_handle host_context_handle,
+                                                                   enum hostfxr_delegate_type type,
+                                                                   /*out*/ void** delegate);
 
-typedef int32_t(HOSTFXR_CALLTYPE *hostfxr_close_fn)(const hostfxr_handle host_context_handle);
+typedef int32_t(HOSTFXR_CALLTYPE* hostfxr_close_fn)(const hostfxr_handle host_context_handle);
 
 #endif //__HOSTFXR_H__
