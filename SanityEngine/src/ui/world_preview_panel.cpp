5970e63a5e1e105ff142ab63b55bf932023c7390
David Dubois <dexcelstraun7@gmail.com>
2020-08-31 00:52:06 -0700

    A note to future DethRaid

--- SanityEngine/src/ui/world_preview_panel.cpp
+++ SanityEngine/src/ui/world_preview_panel.cpp
@@ -3,7 +3,9 @@
 #include "rhi/render_device.hpp"
 
 namespace ui {
-    WorldPreviewPane::WorldPreviewPane(const renderer::Renderer& renderer_in) : renderer{&renderer_in} {}
+    WorldPreviewPane::WorldPreviewPane(const renderer::Renderer& renderer_in) : renderer{&renderer_in} {
+        // TODO: Get the texture index for Scene output render texture
+    }
 
     void WorldPreviewPane::draw() {
         if(ImGui::Begin("World Preview")) {
