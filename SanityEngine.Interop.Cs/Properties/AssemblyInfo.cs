33096d9d0ce337f0965f5304dad5f6c09e09acff
David Dubois <dexcelstraun7@gmail.com>
2020-09-03 09:47:14 -0700

    Continually mess around with msbuild

--- SanityEngine.Interop.Cs/Properties/AssemblyInfo.cs
+++ SanityEngine.Interop.Cs/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,10 +7,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SanityEngine.Interop.Cs")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("C# language projections for the Sanity Engine")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SanityEngine.Interop.Cs")]
+[assembly: AssemblyCompany("Jotun Studios")]
+[assembly: AssemblyProduct("Sanity Engine")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -27,3 +28,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
