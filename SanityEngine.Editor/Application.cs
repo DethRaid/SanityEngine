46241f913e1fab1e489847634948c5e9bbd05ef5
David Dubois <dexcelstraun7@gmail.com>
2020-09-04 00:35:10 -0700

    Non-UWP application shell

---
+++ SanityEngine.Editor/Application.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SanityEngine.Editor
+{
+    /// <summary>
+    /// Application class for the Sanity Engine world editor
+    /// </summary>
+    class Application
+    {
+        static void Main(string[] _)
+        {
+            Console.WriteLine("Hello World!");
+
+            Sanity.SanityEngine engine = new();
+
+            engine.Tick(0.5);
+        }
+    }
+}
