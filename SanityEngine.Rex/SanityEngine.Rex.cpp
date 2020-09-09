093fbebbaa16a0ae4b09f45e6ca6d185af8894e7
David Dubois <dexcelstraun7@gmail.com>
2020-09-08 18:18:09 -0700

    Wrap up Rex in its own static library, so it's easier to use in other projects

---
+++ SanityEngine.Rex/SanityEngine.Rex.cpp
@@ -0,0 +1,10 @@
+// SanityEngine.Rex.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnSanityEngineRex()
+{
+}
