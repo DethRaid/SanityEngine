ea0751b33fe05cfbd6bd809b08f4f67c3c4f1d8b
David Dubois <dexcelstraun7@gmail.com>
2020-09-10 00:18:39 -0700

    Committing quick before I break things worse

--- SanityEngine.Editor.UWP/Properties/AssemblyInfo.cs
+++ SanityEngine.Editor.UWP/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SanityEngineEditor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Sanity.Editor")]
+[assembly: AssemblyDescription("Sanity Engine world editor")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SanityEngineEditor")]
+[assembly: AssemblyCompany("Jotun Studios")]
+[assembly: AssemblyProduct("Sanity.Editor")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -27,3 +28,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
---
+++ SanityEngine.Editor.UWP/UI/MainPage.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+using Microsoft.Toolkit.Uwp.UI.Controls;
+
+using SanityEngineEditor.UI.Project;
+
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using WinUIMenuBarItem = Microsoft.UI.Xaml.Controls.MenuBarItem;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Sanity.Editor
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public void NewProject_Click(object sender, RoutedEventArgs e)
+        {
+            // Show the user the project creation wizard
+
+            // Frame rootFrame = Window.Current.Content as Frame;
+
+            // No parameters - the page should not get populated with any data, allowing the user to fill out their project's information
+            // rootFrame.Navigate(typeof(EditProjectPage), default);
+        }
+
+        public void SaveProject_Click(object sender, RoutedEventArgs e)
+        {
+            // Save all files in the project
+        }
+
+        public void OpenProject_Click(object sender, RoutedEventArgs e)
+        {
+            // Show the user the project launcher, which lets them launch any project that Sanity Engine is aware of 
+            // (or select a project folder on their own)
+        }
+
+        public void EditProject_Click(object sender, RoutedEventArgs e)
+        {
+            // Open a dialog to edit project settings
+        }
+
+        public void EditWorld_Click(object sender, RoutedEventArgs e)
+        {
+            // Open a dialog to edit world settings
+        }
+
+        public void NewAiSense_Click(object sender, RoutedEventArgs e)
+        {
+            // Open a dialog to add a new AI sense
+        }
+
+        public void EditAiSense_Click(object sender, RoutedEventArgs e)
+        {
+            // Open a dialog to let the user select an AI Sense to edit
+        }
+
+        public void NewAiBehaviorTree_Click(object sender, RoutedEventArgs e)
+        {
+            // Open a dialog to add a behavior tree
+        }
+
+        public void EditAiBehaviorTree_Click(object sender, RoutedEventArgs e)
+        {
+            // Open a dialog to let the user select a behavior tree to edit
+        }
+    }
+}
