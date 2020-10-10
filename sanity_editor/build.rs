6c4e83346d81bd44d2112787c25ffde8290fcbaf
David Dubois <dexcelstraun7@gmail.com>
2020-10-10 16:37:09 -0700

    Add Rust editor

---
+++ sanity_editor/build.rs
@@ -0,0 +1,19 @@
+fn main() {
+    compile_cpp_engine();
+
+    generate_rust_bindings();
+
+    compile_rust_editor();
+}
+
+fn compile_cpp_engine() {
+    unimplemented!()
+}
+
+fn generate_rust_bindings() {
+    unimplemented!()
+}
+
+fn compile_rust_editor() {
+    unimplemented!()
+}
