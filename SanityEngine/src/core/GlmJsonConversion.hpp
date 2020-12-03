52c0cdda5c1a595695e6a40f9b735d8703d274dd
David Dubois <dexcelstraun7@gmail.com>
2020-12-02 22:21:28 -0800

    Begin sorting out JSON conversion

---
+++ SanityEngine/src/core/GlmJsonConversion.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "core/JsonConversion.hpp"
+#include "glm/glm.hpp"
+
+namespace glm {
+    NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(vec2, x, y)
+    NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(vec3, x, y, z)
+    NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(vec4, x, y, z, w)
+    NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(quat, x, y, z, w)
+}
