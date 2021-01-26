b5c97e7a1037b35d0c15b83a45ccd87d2754b7ad
David Dubois <dexcelstraun7@gmail.com>
2021-01-25 18:36:41 -0800

    Fix normals importing

--- Sanity.Editor/src/import/scene_importer.cpp
+++ Sanity.Editor/src/import/scene_importer.cpp
@@ -548,7 +548,7 @@ namespace sanity::editor::import {
                 texcoord.y = 1.0f - texcoord.y; // Convert OpenGL-style texcoords to DirectX-style
             }
 
-            vertices.push_back(StandardVertex{.location = location, .normal = {normal.x, -normal.y, normal.z}, .texcoord = texcoord});
+            vertices.push_back(StandardVertex{.location = location, .normal = {normal.x, normal.y, -normal.z}, .texcoord = texcoord});
 
             position_read_ptr++;
             normal_read_ptr++;
